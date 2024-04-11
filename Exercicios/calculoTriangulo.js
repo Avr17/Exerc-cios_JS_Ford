38fc281f4c1ee789de0d7658badc93f28c3a76ab
Airogla <airogla17@gmail.com>
2024-04-10 21:45:40 -0300

    ANDAMENTO

--- Exercicios/calculoTriangulo.js
+++ Exercicios/calculoTriangulo.js
@@ -5,8 +5,6 @@
 // − Triângulo    Escaleno:    possui    3    lados    diferentes.
 
 
-
-
 //entrada de dados
 // = informar as 3 medidas de um triangulo
 
@@ -17,3 +15,32 @@
 //saida de dados
 // = informar se o triangulo é equilátero, escaleno ou isócele
 // = motrar a saída do programa na console
+
+<Script>
+//Programa para identificar o tipo de triángulo 
+
+// Criamos a função para ler as medidas e determinar o tipo de triángulo 
+
+function tipoTriángulo (lado1, lado2,lado3){
+
+    if (lado1 === lado2 && lado2 === lado3) {
+                return "Equilátero";
+    } else if (lado1 === lado2 || lado2 === lado3 || lado1 === lado3) {
+                return "Isósceles";
+    } else {
+                return "Escaleno";
+}
+}
+
+// Entrada de dados
+const lado1 = parseFloat(prompt("Informe a medida do primeiro lado:"));
+const lado2 = parseFloat(prompt("Informe a medida do segundo lado:"));
+const lado3 = parseFloat(prompt("Informe a medida do terceiro lado:"));
+
+//Saída de dados
+ const resultado = tipoTriangulo(lado1, lado2, lado3);
+ console.log(O triângulo é ${resultado}.);
+
+ 
+
+</Script>
